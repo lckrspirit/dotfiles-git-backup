8f5d78ad05f83de187ec5564ffe32e27bdb17b45
Lkcrspirit <lckrspiritus@gmail.com>
2022-01-08 21:52:16 +0600

    Update repo ..

--- README.md
+++ README.md
@@ -1,8 +1,10 @@
 # dotfiles-git-backup
 My dotfiles backup, and shell script for backup config's.
 
-**folders:**
+**folders, configs:**
 - ThinkpadT470 - dotfiles, from Thinkpad, 
 - Arch-homepc - dotfiles, from my home desktop.
-
+    - Folder `i3block-scripts` - simple scripts for i3block elements
+    - i3wm main config - `config`
+    - i3block bar config - `i3blocks.conf`
 
--- dotfiles-backup.sh
+++ dotfiles-backup.sh
@@ -2,16 +2,24 @@
 
 device=$(hostnamectl hostname)
 configSourcePath="$HOME/.config/i3"
-configBackupPath="/mnt/media/scripts/dotfiles-git-backup/$device"
+configBackupPath="/mnt/media/scripts/dotfiles-git-backup"
+commitComment=""
 
-
-if [[ ! -d "$configBackupPath" ]]; then
-    mkdir "$configBackupPath"
+if [[ ! -d "$configBackupPath"/"$device" ]]; then
+    mkdir "$configBackupPath"/"$device"
 fi
 
 
 function copyFiles {
     yes | cp -rfv "$configSourcePath"/* "$configBackupPath"
+}
+
+function pushtoRepo {
+    if [[ ${#commitComment} -eq 0 ]]; then
+        commitComment="Autobackup configs"
+    fi
+    
+    
 }
 
 copyFiles
